07e56dedbf1814efb1c13798a8083adf99ed1c0f
Tomas Suarez <tomas.suarez@anima.edu.uy>
2024-07-17 08:40:50 -0300

    2

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
-import express, { Request, Response } from 'express';
+import express from 'express';
 import routes from './routes';
+import dotenv from 'dotenv';
+
+dotenv.config();  // Cargar las variables de entorno desde el archivo .env
 
 const app = express();
 const port = process.env.PORT || 3000;
