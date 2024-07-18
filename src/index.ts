6e8b0245c02111887a669e16ee1a04db779a47b7
Tomas Suarez <tomas.suarez@anima.edu.uy>
2024-07-17 23:30:30 -0300

    Configuracion del cors para conectar back y front

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,25 @@
 import express from 'express';
 import routes from './routes';
 import dotenv from 'dotenv';
+import cors from 'cors';
 
 dotenv.config();  // Cargar las variables de entorno desde el archivo .env
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(cors({
+  origin: 'http://localhost:3000', // La URL del frontend
+  credentials: true
+}));
 app.use(express.json());
 app.use('/', routes);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+
+
+
+
+
